525bdf83193a897f903827452a91ff30df9d22a9
Hassan Yusuf <hassanyousuf1999@gmail.com>
2021-09-24 10:30:36 -0400

    initial commit for homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import NavMenu from "./components/navmenu";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!!!</h1>
+      <App />
+      <NavMenu />
     </div>
   );
 }
