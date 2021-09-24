2971e7d04e7f024c5b12a4e74af6fd131b8b38de
Hassan Yusuf <hassanyousuf1999@gmail.com>
2021-09-24 12:27:15 -0400

    Info component created

---
+++ client/src/components/Info.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function Info() {
+  return (
+     <div className='info'>
+         <div className='container'>
+             <h1 className='info-heading'>
+             <span>Welcome to DigiArt</span>
+             <br />
+             <span>Buy, Auction & Sell</span>
+             </h1>
+         </div>
+     </div>
+  );
+}
