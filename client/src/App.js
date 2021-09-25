8c9fbf5c607759ecf64986b5646f68b3bbad74c5
Burak Aksu <sabburak26@gmail.com>
2021-09-25 11:48:22 -0400

    added profile page with apollo client and provider

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,36 @@ import NavMenu from "./components/NavMenu";
 import Info from './components/Info';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Router>
         <NavMenu />
         <Switch>
@@ -23,7 +49,7 @@ export default function App() {
       <Info />
       <SignupForm />
       <LoginForm />
-    </>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { useQuery } from '@apollow/client';
+import { QUERY_USER } from '../utils/queries';
+
+function Profile() {
+  const { data } = useQuery(QUERY_USER);
+  let user;
+
+  if (data) {
+    user = data.user;
+  }
+
+  return (
+    <>
+      <div className="container my-1">
+        <Link to="/">← Back to Products</Link>
+
+        {user ? (
+          <>
+            <h1>Hello {user.username}</h1>
+            <h2>
+              Order History for {user.username};
+            </h2>
+            {user.orders.map((order) => (
+              <div key={order._id} className="my-2">
+                <h3>
+                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+                </h3>
+                <div className="flex-row">
+                  {order.products.map(({ _id, image, name, price }, index) => (
+                    <div key={index} className="card px-1 py-1">
+                      <Link to={`/products/${_id}`}>
+                        <img alt={name} src={`/images/${image}`} />
+                        <p>{name}</p>
+                      </Link>
+                      <div>
+                        <span>${price}</span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </>
+        ) : <h1>Oops! No user was found.</h1>}
+      </div>
+    </>
+  );
+}
+
+export default Profile
