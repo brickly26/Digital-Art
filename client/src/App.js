ae5ce541fa280df2b3c83261884c9ba4072ce2aa
James Dobbs <dobbs282@gmail.com>
2021-09-25 12:36:08 -0400

    cart create with actions to remove and checkout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NavMenu from "./components/NavMenu";
+import NavMenu from './components/NavMenu';
 import Info from './components/Info';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
@@ -10,6 +10,8 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import Cart from './components/Cart';
+import { StoreProvider } from './utils/GlobalState';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -33,24 +35,21 @@ const client = new ApolloClient({
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <NavMenu />
-        <Switch>
-          <Route path="/about">
-          </Route>
-          <Route path="/contact">
-          </Route>
-          <Route path="/projects">
-          </Route>
-          <Route path="/">
-          </Route>
-        </Switch>
-      </Router>
-      <Info />
-      <SignupForm />
-      <LoginForm />
+      <StoreProvider>
+        <Router>
+          <NavMenu />
+          <Switch>
+            <Route path="/about"></Route>
+            <Route path="/contact"></Route>
+            <Route path="/projects"></Route>
+            <Route path="/"></Route>
+          </Switch>
+        </Router>
+        <Info />
+        <SignupForm />
+        <LoginForm />
+        <Cart />
+      </StoreProvider>
     </ApolloProvider>
   );
 }
-
-
---
+++ client/src/components/CartItem/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART } from '../../utils/actions';
+import { idbPromise } from '../../utils/helper';
+
+const CartItem = ({ item }) => {
+  const [_, dispatch] = useStoreContext();
+
+  const removeFromCart = (item) => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+      sold: false,
+    });
+    idbPromise('cart', 'delete', { ...item });
+  };
+
+  return (
+    <div className="flex-row">
+      {/* if images needed add */}
+      {/* <div>
+        <img src={`/images/${item.image}`} alt="" />
+      </div> */}
+      <div>
+        <div>
+          {item.name}, ${item.price}
+        </div>
+        <div>
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
+            ✘
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
