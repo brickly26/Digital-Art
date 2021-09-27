7eed8c04e77accd7932a484cd6599dae6c375926
James Dobbs <dobbs282@gmail.com>
2021-09-27 13:48:11 -0400

    addtocart works

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,22 +1,16 @@
 import React, { useEffect } from 'react';
 import { useStoreContext } from '../utils/GlobalState';
 import { useParams } from 'react-router-dom';
-import ProductItem from './ProductItem'
+import ProductItem from './ProductItem';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../utils/queries';
-import { idbPromise } from '../utils/helper';
 
 export default function Category() {
-  const [state, dispatch] = useStoreContext();
   const { category } = useParams();
 
-  const { currentCategory } = state;
-  console.log("id is:", category)
-
   const { loading, data } = useQuery(QUERY_PRODUCTS, {
-    variables: { category: category }
+    variables: { category: category },
   });
-  console.log(data);
 
   return (
     <div className="my-2">
@@ -40,5 +34,5 @@ export default function Category() {
       )}
       {loading ? <h2>Loading: please hold</h2> : null}
     </div>
-  )
+  );
 }
