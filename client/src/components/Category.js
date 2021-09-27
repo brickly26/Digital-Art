a2986f8bd11e7dbb7c56166aec60c5d9e1ef18b6
Burak Aksu <sabburak26@gmail.com>
2021-09-27 10:57:13 -0400

    pulled main

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useStoreContext } from '../utils/GlobalState';
+import { ProductItem } from '/ProductItem'
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
@@ -12,6 +13,25 @@ function Categories() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   return (
-    <div className=
+    <div className="my-2">
+      <h2>Our Products:</h2>
+      {state.products.length ? (
+        <div className="flex-row">
+          {filterProducts().map((product) => (
+            <ProductItem
+              key={product._id}
+              _id={product._id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              quantity={product.quantity}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3>You haven't added any products yet!</h3>
+      )}
+      {loading ? <h2>Loading: please hold</h2> : null}
+    </div>
   )
 }
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -9,7 +9,6 @@ const StoreProvider = ({ value = [], ...props }) => {
     products: [],
     cart: [],
     cartOpen: false,
-    categories: [],
     currentCategory: '',
   });
 
